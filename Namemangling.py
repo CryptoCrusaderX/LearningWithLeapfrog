1c56dcb469b371d2ecd711356c165f2261a7b086
CryptoCrusaderX <kandelkismat005@gmail.com>
2025-06-05 21:43:27 +0545

    Name Mangling JUNE 05

--- Namemangling.py
+++ Namemangling.py
@@ -5,6 +5,6 @@ class Product:
 
 
 product = Product("Cetaphil", 1750)
-
+# Acessing Private attribute from outside of the class only with the help of NAME MANGLING
 print(product._Product__name)
 print(product._Product__price)
