d7161ec323bbe635d9be37078720439c5b70f306
CryptoCrusaderX <kandelkismat005@gmail.com>
2025-06-09 22:23:30 +0545

    Polymorphism with Inheritance

---
+++ Poly.py
@@ -0,0 +1,25 @@
+class Payment:
+    def process_payment(self):
+        return "Processing Generic Payment!"
+
+
+class CreditCardPayment(Payment):
+    def process_payment(self):
+        return "Processing Credit Card Payment!"
+
+
+class PayPalPayment(Payment):
+    def process_payment(self):
+        return "Processing PayPal Payment"
+
+
+# Polymorphic Function!
+def execute_payment(payment_method):
+    print(payment_method.process_payment())
+
+
+credit_card = CreditCardPayment()
+paypal = PayPalPayment()
+
+execute_payment(credit_card)
+execute_payment(paypal)
