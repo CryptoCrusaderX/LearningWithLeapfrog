50bdb500fec0c2beaf1d9ad135ba9909d6871c6f
CryptoCrusaderX <kandelkismat005@gmail.com>
2025-06-10 22:45:20 +0545

    Abstraction JUNE 10

---
+++ Abstractiontask.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+
+shapes = [Circle(8), Rectangle(7, 9), Square(9)]
+
+for shape in shapes:
+    print(f"{shape.__class__.__name__}:")
+    print(f"    Area:{shape.area():.2f}")
+    print(f"    Perimeter:{shape.perimeter():.2f}")
