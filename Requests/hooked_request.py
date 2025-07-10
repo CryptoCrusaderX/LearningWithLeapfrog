cd8a458b6f17d6758b081c0887f66ced96ce0024
CryptoCrusaderX <kandelkismat005@gmail.com>
2025-07-10 21:00:29 +0545

    Event Hooks JULY 10

---
+++ Requests/hooked_request.py
@@ -0,0 +1,36 @@
+import requests
+
+url = "https://httpbin.org/get"
+
+
+def custom_get(url, hooks=None):
+    print(f"Sending GET requests to:{url}")
+    response = requests.get(url)
+
+    if hooks:
+        for hook in hooks:
+            try:
+                hook(response)
+            except Exception as e:
+                print(f"Error in hooks:{e}")
+
+    return response
+
+
+def print_url_hook(resp):
+    print("Hook Triggered. URL was:", resp.url)
+
+
+def check_status_hook(resp):
+    if resp.status_code == 200:
+        print("Status OK(200)")
+    else:
+        print("Something went Wrong!")
+
+
+if __name__ == "__main__":
+    response = custom_get(
+        "https://httpbin.org/get", hooks=[print_url_hook, check_status_hook]
+    )
+
+    print("Response content length:", len(response.text))
