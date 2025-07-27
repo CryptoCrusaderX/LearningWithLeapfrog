cb9182ee8ee7b7dfe8ce9fffeaf6461575f5474f
CryptoCrusaderX <kandelkismat005@gmail.com>
2025-07-27 22:32:32 +0545

    Public OAuth2

--- Requests/Public_Oauth2.py
+++ Requests/Public_Oauth2.py
@@ -1,11 +1,10 @@
 import requests
 
-# Auth0 public test tenant
+
 token_url = "https://dev-xn4ny5n8.us.auth0.com/oauth/token"
 client_id = "NOQIQYt2AXiF3P7ZOk9y6CgENAX3H1R0"
 client_secret = "xs0xU4rjA_5M6sM7BZH4Y0PLT9OnrJtFn6eGAnpIEvWNz"
 
-# Make token request
 data = {
     "grant_type": "client_credentials",
     "client_id": client_id,
@@ -17,7 +16,7 @@ response = requests.post(token_url, json=data)
 
 if response.status_code == 200:
     token = response.json()["access_token"]
-    print("✅ Access token:", token[:50], "...")
+    print("Access token:", token[:50], "...")
 else:
     print("Failed:", response.status_code)
     print(response.text)
